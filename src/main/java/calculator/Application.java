1570de97fc18b1b0ed9e7e8aa4d4d99d3fae4760
miniminjae92 <rkdalswoals@gmail.com>
2025-10-19 22:35:51 +0900

    feat: 애플리케이션 로직 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,11 +1,25 @@
 package calculator;
 
+import calculator.domain.Calculator;
+import calculator.domain.Converter;
+import calculator.domain.Delimiter;
 import calculator.view.InputView;
+import calculator.view.OutputView;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView();
         String input = inputView.getString();
-        System.out.println(input);
+
+        Delimiter delimiter = new Delimiter();
+        List<String> seperated = delimiter.seperateString(input);
+
+        Converter converter = new Converter();
+        List<Integer> numbers = converter.convertPositiveNumbers(seperated);
+
+        Calculator calculator = new Calculator();
+        OutputView outputView = new OutputView();
+        outputView.printResult(calculator.sum(numbers));
     }
 }
---
+++ src/main/java/calculator/view/OutputView.java
@@ -0,0 +1,8 @@
+package calculator.view;
+
+public class OutputView {
+
+    public void printResult(long sum) {
+        System.out.println("결과 : " + sum);
+    }
+}
