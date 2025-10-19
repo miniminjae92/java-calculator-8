4f542a52bec57908ade9e921ac05ed10c824f5df
minjae kang <miniminjae@ijuhyeon-ui-iMac.local>
2025-10-19 20:24:51 +0900

    feat: Delimiter 테스트 및 클래스 구현

---
+++ src/main/java/calculator/domain/Delimiter.java
@@ -0,0 +1,38 @@
+package calculator.domain;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class Delimiter {
+
+    private static final Set<String> DEFAULT_DELIMITERS = Set.of(",", ":");
+    private static final String START_FLAG = "//";
+    private static final String END_FLAG = "\\\\n";
+    private static final String REGEX_CUSTOM = START_FLAG + "(.*?)" + END_FLAG + "(.*)";
+    private static final Pattern CUSTOM_DELIMITER = Pattern.compile(REGEX_CUSTOM);
+    private final Set<String> delimiters;
+
+    public Delimiter() {
+        this.delimiters = new HashSet<>(DEFAULT_DELIMITERS);
+    }
+
+    public List<String> seperateString(String string) {
+        String checked = checkCustomDelimiter(string);
+        String regex = delimiters.stream().map(Pattern::quote).collect(Collectors.joining("|"));
+        return List.of(checked.split(regex));
+    }
+
+    private String checkCustomDelimiter(String string) {
+        Matcher matcher = CUSTOM_DELIMITER.matcher(string);
+        if (matcher.find()) {
+            delimiters.add(matcher.group(1));
+            return matcher.group(2);
+        }
+        return string;
+    }
+
+}
